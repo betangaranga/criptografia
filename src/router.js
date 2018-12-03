9bc5cc2b8bb1d0569459d9f554e9bacfd29ea725
Angel Aguilera <albertoangelaguilera@gmail.com>
2018-12-03 00:44:11 -0600

    Se agrega uso seguro de sesion

--- src/router.js
+++ src/router.js
@@ -52,12 +52,12 @@ let router=new Router({
     }
   ]
 })
-/*router.beforeEach((to, from, next) => {
+router.beforeEach((to, from, next) => {
   let currentUser = firebase.auth().currentUser;
   let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
 
   if (requiresAuth && !currentUser) next('login')
   else if (!requiresAuth && currentUser) next('principal')
   else next()
-})*/
+})
 export default router
