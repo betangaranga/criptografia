2ccfad0982b4e3123c1a37830e757607ea7402fc
Angel Aguilera <albertoangelaguilera@gmail.com>
2018-12-03 01:24:14 -0600

    Se corrige problemas en salir

--- src/components/Principal.vue
+++ src/components/Principal.vue
@@ -59,7 +59,7 @@ return{
     
        salir: function() {
       firebase.auth().signOut().then(() => {
-        this.$router.replace('/')
+        this.$router.replace('/login')
       })
     }
   },
